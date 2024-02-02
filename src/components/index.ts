fad80141fe6f27fa0cb88a7fd93f5bf5357a9c24
Chea_Pinglong <cheapinglong312@gmail.com>
2024-02-02 11:20:33 +0700

    add storybook

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { ClientComponent, ClientCounter } from "./client";
+
+export { ClientComponent, ClientCounter };
+import { Card } from "./Card";
+export { Card };
+
+export * from "./atoms"
--- src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-import { ClientComponent, ClientCounter } from "./client";
-
-export {ClientComponent, ClientCounter}
-import {Card} from "./Card"
-export {Card}
