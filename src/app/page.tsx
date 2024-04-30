3b6449c171f0e8fd46d3b03afd13fd2dd5a77e7c
Chea_Pinglong <cheapinglong312@gmail.com>
2024-04-30 13:53:49 +0700

    start form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,20 @@ import React from "react";
 const page = () => {
   return (
     <div className=" w-screen h-screen flex justify-center items-center">
-      <div>
+      <div className="flex flex-col">
      
         <Link
           href={"/user"}
           className="bg-blue-500 px-4 py-2 rounded font-bold hover:bg-blue-700 text-white text-3xl"
         >
           To user
+        </Link>
+
+        <Link
+          href={"/form"}
+          className="bg-green-500 px-4 py-2 rounded font-bold  text-white text-3xl"
+        >
+          To form
         </Link>
       </div>
     </div>
